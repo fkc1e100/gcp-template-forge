4ab158b778776e400aa77b88ab542fe2ead082df
Frank Currie <78167014+fkc1e100@users.noreply.github.com>
2026-04-07 17:53:24 -0400

    Disable deletion protection for CI

--- templates/6-enterprise-gke/Terraform_HELM/terraform/main.tf
+++ templates/6-enterprise-gke/Terraform_HELM/terraform/main.tf
@@ -1,6 +1,7 @@
 resource "google_container_cluster" "enterprise_cluster" {
   name     = var.cluster_name
   location = var.region
+  deletion_protection = false
 
   # We can't create a cluster with no node pool defined, but we want to only use
   # separately managed node pools. So we create the smallest possible default
